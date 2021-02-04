203bbd9adf315b7a90b584e3929bb279c853a8fa
Vita Khutorianska <steblvita@gmail.com>
2021-02-04 13:01:27 +0200

    color random done

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const colors = [
   '#009688',
   '#795548',
 ];
-const randomIntegerFromInterval = (min, max) => {
+const refs = {startBtn: document.querySelector('button[data-action="start"]'),
+stopBtn: document.querySelector('button[data-action="stop"]'),
+ body: document.querySelector('body'),}
+
+
+let timerId = null;
+
+  const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+refs.startBtn.addEventListener('click', colorRandom);
+function colorRandom() {
+refs.startBtn.disabled=true;
+  timerId = setInterval(() => {
+  refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length-1)]   
+    
+  }, 1000)
+}
+refs.stopBtn.addEventListener('click', randomColorStop);
+function randomColorStop() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false
+  
+}
